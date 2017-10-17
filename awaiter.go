cd7edd0bf8017a6de5038d2bb2f9d1839f2d2bdc
dalixu <you@example.com>
2017-10-17 09:07:48 +0800

    init

---
+++ awaiter.go
@@ -0,0 +1,195 @@
+package awaiter
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+//Awaiter Awaiter接口
+type Awaiter interface {
+	Await() (interface{}, error)
+	Waiter() <-chan bool
+	IsFinished() bool
+	ContinueWith(do ContinueDo, arg ...interface{}) Awaiter
+	Cancel() Awaiter
+	CancelWaiter() <-chan bool
+	IsCancellationRequested() bool
+}
+
+//Do Awaiter的调用函数类型
+type Do func(aw Awaiter, arg ...interface{}) (interface{}, error)
+
+//ContinueDo continue的调用函数类型
+type ContinueDo func(aw Awaiter, prev Awaiter, arg ...interface{}) (interface{}, error)
+
+//New 返回Awaiter
+func New(do Do, arg ...interface{}) Awaiter {
+	return newCommonAwaiter().async(do, arg...)
+}
+
+//CommonAwaiter 通用的Awaiter封装goroutine以便能够简单的返回值
+//New(...).Await()
+type CommonAwaiter struct {
+	finishedChan    chan bool
+	reason          error       //结束时返回的错误码
+	result          interface{} //结束时返回的RoutineDo结果
+	cancelChan      chan bool
+	continueMutex   *sync.Mutex
+	continueAwaiter *CommonAwaiter
+	continueDo      Do
+	continueArg     []interface{}
+}
+
+func newCommonAwaiter() *CommonAwaiter {
+	return &CommonAwaiter{
+		finishedChan:    make(chan bool),
+		reason:          nil,
+		result:          nil,
+		cancelChan:      make(chan bool),
+		continueMutex:   &sync.Mutex{},
+		continueAwaiter: nil,
+		continueDo:      nil,
+		continueArg:     nil,
+	}
+}
+
+//Await 等待Awaiter返回
+func (aw *CommonAwaiter) Await() (interface{}, error) {
+	<-aw.finishedChan
+	return aw.result, aw.reason
+}
+
+//Waiter 返回管道 方便在select中等待
+//use: case <- Waiter() not case v := <-Waiter()
+//因为运行结束后直接Close了 并没有写入值 这里把Chan当作纯粹的bool
+func (aw *CommonAwaiter) Waiter() <-chan bool {
+	return aw.finishedChan
+}
+
+//IsFinished RoutineDo 是否结束
+func (aw *CommonAwaiter) IsFinished() bool {
+	select {
+	case <-aw.finishedChan:
+		return true
+	default:
+		return false
+	}
+}
+
+//ContinueWith 方便写pipeline 注意 do运行时参数传递时会把之前的Awaiter放到arg前 一起传给do
+func (aw *CommonAwaiter) ContinueWith(do ContinueDo, arg ...interface{}) Awaiter {
+	aw.continueMutex.Lock()
+	defer aw.continueMutex.Unlock()
+	//已经注册过continueWith函数 禁止再注册
+	if aw.continueAwaiter != nil {
+		return nil
+	}
+	aw.continueAwaiter = newCommonAwaiter()
+	param := make([]interface{}, 0, len(arg)+1)
+	param = append(append(param, aw), arg...)
+	aw.continueDo = func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return do(aw, arg[0].(Awaiter), arg[1:]...)
+	}
+	aw.continueArg = param
+	if aw.IsFinished() {
+		aw.continueAwaiter.async(aw.continueDo, aw.continueArg...)
+	}
+	return aw.continueAwaiter
+}
+
+//Cancel 要求AwaiterDo尽快cancel退出 但并不保证RoutineDo以Cancel退出
+//只能调用一次
+func (aw *CommonAwaiter) Cancel() Awaiter {
+	close(aw.cancelChan)
+	return aw
+}
+
+//CancelWaiter 返回撤销管道 方便在select中使用
+func (aw *CommonAwaiter) CancelWaiter() <-chan bool {
+	return aw.cancelChan
+}
+
+//IsCancellationRequested AwaiterDo应该不时的调用这个函数以便知道自己是否需要及时退出 如果返回true应该尽快退出
+func (aw *CommonAwaiter) IsCancellationRequested() bool {
+	select {
+	case <-aw.cancelChan: //close 或者有值的话 这里会直接返回
+		return true
+	default:
+		return false
+	}
+}
+
+func (aw *CommonAwaiter) async(do Do, arg ...interface{}) *CommonAwaiter {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				if e, ok := r.(error); ok {
+					aw.setResult(nil, e)
+				} else {
+					aw.setResult(nil, fmt.Errorf("Awaiter:Unknown Error:%v", r))
+				}
+			}
+			close(aw.finishedChan)
+			aw.continueMutex.Lock()
+			if aw.continueAwaiter != nil {
+				aw.continueAwaiter.async(aw.continueDo, aw.continueArg...)
+			}
+			aw.continueMutex.Unlock()
+		}()
+		aw.setResult(do(aw, arg...))
+	}()
+	return aw
+}
+
+func (aw *CommonAwaiter) setResult(result interface{}, err error) {
+	aw.reason = err
+	aw.result = result
+}
+
+//CollectionAwaiter Awaiter的集合用于处理多个Awaiter
+type CollectionAwaiter struct {
+	*CommonAwaiter
+	children []Awaiter
+}
+
+//Cancel 覆盖CommonAwaiter Cancel
+func (ca *CollectionAwaiter) Cancel() Awaiter {
+	close(ca.cancelChan)
+	for _, c := range ca.children {
+		c.Cancel()
+	}
+
+	return ca
+}
+
+//WhenAny 有1个完成则返回完成的Awaiter
+func WhenAny(aw ...Awaiter) Awaiter {
+	parent := &CollectionAwaiter{CommonAwaiter: newCommonAwaiter(), children: aw}
+	parent.async(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		aws := arg[0].([]Awaiter)
+		for {
+			for _, a := range aws {
+				if a.IsFinished() {
+					return a, nil
+				}
+			}
+			runtime.Gosched()
+		}
+	}, parent.children)
+
+	return parent
+}
+
+//WhenAll 等待所有的完成并返回[]Awaiter
+func WhenAll(aw ...Awaiter) Awaiter {
+	parent := &CollectionAwaiter{CommonAwaiter: newCommonAwaiter(), children: aw}
+	parent.async(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		aws := arg[0].([]Awaiter)
+		for _, a := range aws {
+			a.Await()
+		}
+		return aws, nil
+	}, parent.children)
+	return parent
+}
---
+++ awaiter_test.go
@@ -0,0 +1,358 @@
+package awaiter
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestAwaiterPanic(t *testing.T) {
+	v, err := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		panic(errors.New("test panic"))
+	}).Await()
+	if err == nil || v != nil {
+		t.Error("awaiter panic fail")
+	} else {
+		t.Log(err)
+	}
+
+	v, err = New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		panic(100)
+	}).Await()
+	if err == nil || v != nil {
+		t.Error("awaiter panic fail")
+	} else {
+		t.Log(err)
+	}
+}
+
+func TestAwaiterArg(t *testing.T) {
+	//测试aw参数传递是否正确
+	aw := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		if len(arg) == 3 && arg[0].(int) == 1 && arg[1].(int) == 2 && arg[2].(int) == 3 {
+			return true, nil
+		}
+		return false, errors.New("awaiter arg fail")
+	}, 1, 2, 3)
+
+	v, err := aw.Await()
+	if v.(bool) == true && err == nil {
+		t.Log("awater arg success 1")
+	} else {
+		t.Error(err)
+	}
+	//不传递参数
+	aw = New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		if len(arg) == 0 {
+			return true, nil
+		}
+		return false, errors.New("awaiter  send param fail")
+	})
+	v, err = aw.Await()
+	if v.(bool) == true && err == nil {
+		t.Log("awater arg success 2")
+	} else {
+		t.Error(err)
+	}
+	//传递nil参数
+	aw = New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		if len(arg) == 1 && arg[0] == nil {
+			return true, nil
+		}
+		return false, errors.New("awaiter  send param fail")
+	}, nil)
+	v, err = aw.Await()
+	if v.(bool) == true && err == nil {
+		t.Log("awater arg success 3")
+	} else {
+		t.Error(err)
+	}
+}
+
+func TestAwaiterCancel(t *testing.T) {
+	v, err := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		count := 0
+		for {
+			if aw.IsCancellationRequested() {
+				//多次调用NeedCancel不会引起panic
+				aw.IsCancellationRequested()
+				aw.IsCancellationRequested()
+				return nil, errors.New("it's cancel error")
+			}
+			time.Sleep(1 * time.Second)
+			count++
+			if count >= 5 {
+				return nil, errors.New("test cancel fail")
+			}
+		}
+	}).Cancel().Await()
+
+	if err == nil || v != nil {
+		t.Error("awaiter cancel fail")
+	} else {
+		t.Log(err)
+	}
+}
+
+func TestAwaiterCancelWaiter(t *testing.T) {
+	v, err := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+	loop:
+		for {
+			select {
+			case <-aw.CancelWaiter():
+				break loop
+			}
+		}
+		return nil, errors.New("it's cancel error")
+	}).Cancel().Await()
+
+	if err == nil || v != nil {
+		t.Error("awaiter cancel fail")
+	} else {
+		t.Log(err)
+	}
+}
+
+func TestAwaiterResult(t *testing.T) {
+	v, err := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return arg[0].(string), nil
+	}, "1").Cancel().Await()
+
+	if err != nil || v == nil {
+		t.Error("awaiter result fail", err, v)
+	} else {
+		t.Log(v, err)
+	}
+}
+
+func TestAwaiterIsFinished(t *testing.T) {
+	c := make(chan bool, 1)
+	aw := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		select {
+		case <-c:
+		}
+		return true, nil
+	})
+	if aw.IsFinished() {
+		t.Error("awaiter finished fail 0")
+	}
+	if aw.IsFinished() {
+		t.Error("awaiter finished fail 1")
+	}
+	c <- true
+	aw.Await()
+	if !aw.IsFinished() {
+		t.Error("awaiter finished fail 2")
+	}
+	if !aw.IsFinished() {
+		t.Error("awaiter finished fail 3")
+	}
+	v, err := aw.Await()
+	if err != nil || v == nil {
+		t.Error("awaiter result fail")
+	} else {
+		t.Log(v, err)
+	}
+}
+
+func TestAwaiterContinueArg(t *testing.T) {
+	//测试awcontinueWith参数传递是否正确
+	aw := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return nil, nil
+	}).ContinueWith(func(aw Awaiter, prev Awaiter, arg ...interface{}) (interface{}, error) {
+		if len(arg) == 3 && prev.IsFinished() &&
+			arg[0].(int) == 1 && arg[1].(int) == 2 && arg[2].(int) == 3 {
+			return true, nil
+		}
+
+		return false, errors.New("awaiter continuewith send param fail")
+	}, 1, 2, 3)
+	v, err := aw.Await()
+	if v.(bool) == true && err == nil {
+		t.Log("awater continue arg success 1")
+	} else {
+		t.Error(err)
+	}
+	//不传递参数
+	aw = New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return nil, nil
+	}).ContinueWith(func(aw Awaiter, prev Awaiter, arg ...interface{}) (interface{}, error) {
+		if len(arg) == 0 && prev.IsFinished() {
+			return true, nil
+		}
+		return false, errors.New("awaiter continuewith send param fail")
+	})
+	v, err = aw.Await()
+	if v.(bool) == true && err == nil {
+		t.Log("awater continue arg success 2")
+	} else {
+		t.Error(err)
+	}
+	//传递nil参数
+	aw = New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return nil, nil
+	}).ContinueWith(func(aw Awaiter, prev Awaiter, arg ...interface{}) (interface{}, error) {
+		if len(arg) == 1 && prev.IsFinished() &&
+			arg[0] == nil {
+			return true, nil
+		}
+		return false, errors.New("awaiter continuewith send param fail")
+	}, nil)
+	v, err = aw.Await()
+	if v.(bool) == true && err == nil {
+		t.Log("awater continue arg success 3")
+	} else {
+		t.Error(err)
+	}
+}
+
+//测试aw已经完成后 能否执行continueWith
+func TestAwaiterContinueWithIfFinished(t *testing.T) {
+
+	aw := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return nil, nil
+	})
+	aw.Await()
+	c := make(chan bool)
+	aw.ContinueWith(func(aw Awaiter, prev Awaiter, arg ...interface{}) (interface{}, error) {
+		c <- true
+		return nil, nil
+	})
+	select {
+	case <-c:
+		t.Log("awaiter continue success")
+	case <-time.After(2 * time.Second):
+		t.Error("awaiter continue fail")
+	}
+}
+
+//测试aw未完成后 能否执行continueWith
+func TestAwaiterContinueWithIfRunning(t *testing.T) {
+	c := make(chan bool)
+	New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		<-c
+		return nil, nil
+	}).ContinueWith(func(aw Awaiter, prev Awaiter, arg ...interface{}) (interface{}, error) {
+		c <- false
+		return nil, nil
+	})
+	c <- true
+	//aw.Await()
+	select {
+	case v := <-c:
+		if v == false {
+			t.Log("awaiter continue success")
+		} else {
+			t.Error("awaiter continue fail")
+		}
+	case <-time.After(2 * time.Second):
+		t.Error("awaiter continue fail")
+	}
+}
+
+func TestAwaiterWhenAny(t *testing.T) {
+	aw1 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return nil, nil
+	})
+	aw2 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		select {
+		case <-time.After(10 * time.Second):
+			return nil, errors.New("TestAwaiterWhenAny: timeout")
+		case <-aw.CancelWaiter():
+			return nil, nil
+		}
+	})
+	any := WhenAny(aw1, aw2)
+	v, e := any.Await()
+	if e != nil {
+		t.Error(e)
+	} else {
+		if v.(Awaiter) != aw1 {
+			t.Error(e)
+		}
+	}
+}
+
+func TestAwaiterWhenAnyWithCancel(t *testing.T) {
+	aw1 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		select {
+		case <-time.After(10 * time.Second):
+			return nil, errors.New("TestAwaiterWhenAnyWithCancel: timeout")
+		case <-aw.CancelWaiter():
+			return nil, nil
+		}
+	})
+	aw2 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		select {
+		case <-time.After(10 * time.Second):
+			return nil, errors.New("TestAwaiterWhenAnyWithCancel: timeout")
+		case <-aw.CancelWaiter():
+			return nil, nil
+		}
+	})
+	any := WhenAny(aw1, aw2)
+	v, e := any.Cancel().Await() //v可能是aw1也可能aw2
+	if e != nil {
+		t.Error(e)
+	} else {
+		if v.(Awaiter) != aw1 && v.(Awaiter) != aw2 {
+			t.Error(e)
+		}
+		if !v.(Awaiter).IsCancellationRequested() {
+			t.Error(e)
+		}
+	}
+}
+
+func TestAwaiterWhenAll(t *testing.T) {
+	aw1 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		return nil, nil
+	})
+	aw2 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		select {
+		case <-time.After(1 * time.Second):
+			return nil, errors.New("TestAwaiterWhenAll: timeout")
+		case <-aw.CancelWaiter():
+			return nil, nil
+		}
+	})
+	all := WhenAll(aw1, aw2)
+	v, e := all.Cancel().Await() //v可能是aw1也可能aw2
+	if e != nil {
+		t.Error(e)
+	} else {
+		array := v.([]Awaiter)
+		if len(array) != 2 || array[0] != aw1 || array[1] != aw2 {
+			t.Error("TestAwaiterWhenAll fail")
+		}
+	}
+}
+
+func TestAwaiterWhenAllWithCancel(t *testing.T) {
+	aw1 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		select {
+		case <-time.After(10 * time.Second):
+			return nil, errors.New("TestAwaiterWhenAllWithCancel: timeout")
+		case <-aw.CancelWaiter():
+			return nil, nil
+		}
+	})
+	aw2 := New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+		select {
+		case <-time.After(10 * time.Second):
+			return nil, errors.New("TestAwaiterWhenAllWithCancel: timeout")
+		case <-aw.CancelWaiter():
+			return nil, nil
+		}
+	})
+	all := WhenAll(aw1, aw2)
+	v, e := all.Cancel().Await() //v可能是aw1也可能aw2
+	if e != nil {
+		t.Error(e)
+	} else {
+		array := v.([]Awaiter)
+		if len(array) != 2 || array[0] != aw1 || array[1] != aw2 || !aw1.IsCancellationRequested() || !aw2.IsCancellationRequested() {
+			t.Error("TestAwaiterWhenAllWithCancel fail")
+		}
+	}
+}
