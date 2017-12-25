9ea656d51d58ad311cf2962f0d37b8f88cfd0bce
dalixu <506466510@qq.com>
2017-12-25 22:49:06 +0800

    move looper from nscore

---
+++ looper.go
@@ -0,0 +1,50 @@
+package awaiter
+
+import (
+	"fmt"
+	"time"
+)
+
+//Looper 使用awaiter实现一个类似定时器的功能 定时执行DO
+type Looper struct {
+	awaiter Awaiter
+}
+
+//LooperDo Looper的定时执行函数
+type LooperDo func(lp *Looper)
+
+//NewLooper 返回一个Looper
+func NewLooper(dt time.Duration, do LooperDo) *Looper {
+	looper := &Looper{
+		awaiter: nil,
+	}
+	looper.awaiter = New(func(aw Awaiter, arg ...interface{}) (interface{}, error) {
+	loop:
+		for {
+			select {
+			case <-time.After(dt):
+				looper.invoke(do)
+			case <-aw.CancelWaiter():
+				break loop
+			}
+		}
+		return nil, nil
+	})
+	return looper
+}
+
+func (lp *Looper) invoke(do LooperDo) (e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e = fmt.Errorf("%+v", err)
+		}
+	}()
+	do(lp)
+	return nil
+}
+
+//Exit 停止定时器
+func (lp *Looper) Exit() error {
+	_, err := lp.awaiter.Cancel().Await()
+	return err
+}
